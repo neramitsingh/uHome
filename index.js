07a45b4e46a6b0adb4980e27c7a35b758ff25e47
Unknown <neramit.singh@gmail.com>
2019-11-23 15:57:28 +0700

    Change mapping for date to string

--- index.js
+++ index.js
@@ -395,6 +395,7 @@ app.post('/api/starttimer', (req, res) => {
         if (err) res.send(err)
         else {
           objectid = timer._id
+          timer.time = timer.time.toString()
           res.status(200).send(timer)
 
           setTimeout(function (timer) {
