f419eff2d029be5a322d1005c17ff1c03dee44bb
Unknown <neramit.singh@gmail.com>
2020-03-09 22:04:32 +0700

    smart 24

--- smartLearn.js
+++ smartLearn.js
@@ -48,10 +48,10 @@ module.exports.calculateAvg = function(arr, days){
         var total = 0.00;
         for(let i = 0; i < arr.length; i++){
             for(let j = 0; j<arr[i].length; j++){
-                if(i%2 != 0){
+                if(j%2 != 0){
                     console.log("Odd")
-                    console.log(arr[i])
-                    total = total + arr[i]
+                    console.log(arr[i][j])
+                    total = total + arr[i][j]
                     
                 }
             }
